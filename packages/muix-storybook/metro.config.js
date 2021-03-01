ccdbeca66b40e8ef0b6d519db4e459eddfd5542c
monthem <lpoeh01@gmail.com>
2021-03-01 21:34:30 +0900

    chore: removed unnecessary codes in metro.config.js, moved muix-storybook/tsconfig.excludes to /tsconfig.json

--- packages/muix-storybook/metro.config.js
+++ packages/muix-storybook/metro.config.js
@@ -8,22 +8,12 @@ const path = require("path");
  */
 
 module.exports = {
-  projectRoot: path.resolve(__dirname),
   resolver: {
-    sourceExts: ['js', 'jsx', 'ts', 'tsx'],
-    extraNodeModules: new Proxy(
-      {
-        '@muix/muix-components': path.resolve(__dirname, "../muix-components"),
-      },
-      {
-        get: (target, name) => {
-          return path.join(__dirname, `node_modules/${name}`);
-        }
-      }
-    )
+    extraNodeModules: {
+      '@muix/muix-components': path.resolve(__dirname, "../muix-components"),
+    },
   },
   watchFolders: [
-    path.resolve(__dirname, "../../node_modules"),
     path.resolve(__dirname, "../muix-components"),
   ],
   transformer: {
