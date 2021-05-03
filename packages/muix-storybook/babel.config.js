b5587920143a64799717a6903e1ff6c312d57dc2
monthem <lpohe01@gmail.com>
2021-05-03 10:24:36 +0900

    chore: fix webpack.config.js and babel.config.js in muix-storybook

--- packages/muix-storybook/babel.config.js
+++ packages/muix-storybook/babel.config.js
@@ -1,24 +1,11 @@
-const path = require("path");
-
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset'
-  ],
-  plugins: [
-    ['module-resolver', {
-      "extensions": [".js", ".jsx", ".ts", ".tsx"],
-      "alias": {
-        "react": path.resolve(__dirname, "../../node_modules/react"),
-        "@monthem/muix": path.resolve(__dirname, "../muix"),
-        '@monthem/utils': path.resolve(__dirname, "../utils"),
-        '@monthem/web-color': path.resolve(__dirname, "../web-color"),
-        '@monthem/react-native-vector-icons': path.resolve(__dirname, "../react-native-vector-icons"),
-        'react-native-transitional': path.resolve(__dirname, "../react-native-transitional"),
-      }
-    }],
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    ["@babel/plugin-proposal-private-methods", { "loose": true }],
-    ["@babel/plugin-proposal-class-properties", { "loose": true }],
-    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
-  ],
+	presets: [
+		'module:metro-react-native-babel-preset'
+	],
+	plugins: [
+		["@babel/plugin-proposal-decorators", { "legacy": true }],
+		["@babel/plugin-proposal-private-methods", { "loose": true }],
+		["@babel/plugin-proposal-class-properties", { "loose": true }],
+		["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+	],
 };
--- packages/muix-storybook/src/config_web/webpack.config.js
+++ packages/muix-storybook/src/config_web/webpack.config.js
@@ -1,40 +1,41 @@
 const path = require('path')
 const tsLoader = [require.resolve("ts-loader")];
+const rootNodeModules = path.resolve(__dirname, "../../../../node_modules")
 
 if (process.env.USE_DOCGEN === "TRUE") tsLoader.push({
-    loader: require.resolve("react-docgen-typescript-loader"),
-    options: {
-        tsconfigPath: "tsconfig.json"
-    }
+	loader: require.resolve("react-docgen-typescript-loader"),
+	options: {
+		tsconfigPath: "tsconfig.json"
+	}
 })
 
-module.exports = ({ config }) => {
-    config.module.rules.push(
-        {
-            test: /\.(ts|tsx)$/,
-            exclude: /node_modules/,
-            use: [
-                {
-                    loader: 'ts-loader',
-                    options: {
-                        transpileOnly: true
-                    }
-                }
-            ]
-        }
-    );
-    config.resolve.extensions.push(".ts", ".tsx");
-    config.resolve.alias = {
-        'core-js/modules': path.resolve(__dirname, '../../../../node_modules/core-js/modules'),
-        'react': path.resolve(__dirname, '../../../../node_modules/react'),
-        'react-dom': path.resolve(__dirname, '../../../../node_modules/react-dom'),
-        'react-native$': path.resolve(__dirname, '../../../../node_modules/react-native-web'),
-        '@storybook/react-native': path.resolve(__dirname, '../../../../node_modules/@storybook/react'),
-        "@monthem/muix": path.resolve(__dirname, "../../../muix/dist"),
-        "styled-components/native": "styled-components",
-    };
-    config.node = {
-        fs: "empty"
-    };
-    return config;
-};
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'ts-loader',
+						options: {
+							transpileOnly: true
+						}
+					}
+				]
+			}
+		]
+	},
+	resolve: {
+		extensions: [".ts", ".tsx"],
+		alias: {
+			'core-js/modules': path.resolve(rootNodeModules, 'core-js/modules'),
+			'react-native$': path.resolve(rootNodeModules, 'react-native-web'),
+			'@storybook/react-native': path.resolve(rootNodeModules, '@storybook/react'),	
+		}
+	},
+	node: {
+		fs: "empty"
+	}
+}
+
