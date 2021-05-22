43d762c0af1182a0e2175d99687423689099a778
monthem <lpohe01@gmail.com>
2021-05-22 22:29:40 +0900

    add-components: add ChoiceSlicer prototype

--- packages/muix-storybook/src/stories/organisms/layout/choiceSlider.stories.tsx
+++ packages/muix-storybook/src/stories/organisms/layout/choiceSlider.stories.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
-import { StoryFn } from "@storybook/addons"
-import { View } from 'react-native';
+import { Easing, View } from 'react-native';
 import { ChoiceSlider } from '@monthem/muix';
 
-const ChoiceSliderStory: StoryFn<JSX.Element> = (p) => {
+const ChoiceSliderStory = () => {
 	return (
 		<View>
 			<ChoiceSlider
+				alignChunks={"center"}
 				style={{
 					height: 300,
-					backgroundColor: "blue",
+					backgroundColor: "lightgrey",
+				}}
+				choices={[
+					[
+						"I usually",
+						"have been",
+						[
+							{ value: "imaginative", props: { primary: true } },
+							"and",
+							{ value: "romatic", props: { primary: true } },
+						]
+					],
+					["I usually", "have been", { value: "pragmatic and realistic" }],
+					["I usually", "have been", { value: "no no no no no no no no" }],
+				]}
+				chunkInterpolator={({}) => {
+					return {
+						translateX: [-50, 0, 50],
+						translateY: [100, 0, 100],
+					}
+				}}
+				microChunkInterpolator={({ info, chunkIndex, chunks, item, props }) => {
+					const color = props.primary ? "blue" : "black"
+					const fontWeight = props.primary ? "700" : "400"
+					return {
+						opacity: [0, 1, 0],
+						fontSize: [5, 20, 5],
+						color: ["grey", color, "grey"],
+						fontWeight: fontWeight,
+					}
 				}}
 			/>
 		</View>
@@ -18,4 +47,4 @@ const ChoiceSliderStory: StoryFn<JSX.Element> = (p) => {
 }
 
 storiesOf("Organisms/Layout", module)
-	.add("ChoiceSlider", ChoiceSliderStory)
+	.add("ChoiceSlider", () => <ChoiceSliderStory />)
--- packages/muix/src/components/organisms/layout/choice-slider/index.tsx
+++ packages/muix/src/components/organisms/layout/choice-slider/index.tsx
@@ -1,52 +1,405 @@
+import { textStyleProperties } from '@monthem/utils'
 import React from 'react'
-import { View, Text, ViewProps, PanResponder } from 'react-native'
-import { animated, useSpring } from "react-spring/native"
-import Carousel from "react-native-snap-carousel"
+import { TextStyle, View, ViewProps, Text, ViewStyle } from 'react-native'
+import { animated, Interpolation } from "react-spring/native"
+import { SpringConfig, useChain, useSpring, useSpringRef, useSprings, useTrail } from "react-spring"
+import { CarouselBase, CarouselBaseInterpolatorInfo, CarouselBaseRenderItemInfo } from '../../../atoms'
+import { Easing } from 'react-native'
 
-const choices = [
-  "나는 상상력이 풍부하고 낭만적인 편이다",
-  "나는 현실적이고 실용적인 편이다"
-]
+const defaultMicroChunkInterpolator: ChunkInterpolatorFn<any> = ({ info, chunkIndex, chunks, item, props }) => {
+  return {
+    opacity: [0, 1, 0],
+    fontSize: [12, 20, 12],
+    translateX: [-info.layout.width / 2, 0, info.layout.width],
+  }
+}
 
-export const ChoiceSlider = (props: ChoiceSliderProps) => {
-  const [index, setIndex] = React.useState(0);
-  const touchID = React.useRef("-1");
-  const touchStart = React.useRef({x: 0, y: 0}).current;
-  const [spring, setSpring] = useSpring(() => ({
-    virtualTranslate: 0
-  }))
+const defaultChunkInterpolator: ChunkInterpolatorFn<any> = ({ info }) => {
+  return {
+    translateX: [-info.layout.width / 4, 0, info.layout.width / 4],
+    translateY: [50, 0, 50],
+  }
+}
 
-  const panResponder = PanResponder.create({
-    onMoveShouldSetPanResponder: () => true,
-    onPanResponderStart: (e) => {
-      if (touchID.current !== "-1") return;
-      const { pageX, pageY, identifier } = e.nativeEvent;
-      touchID.current = identifier;
-      touchStart.x = pageX;
-      touchStart.y = pageY;
-    },
-    onPanResponderMove: (e) => {
-      const { pageX, locationY, identifier } = e.nativeEvent;
-      if (touchID.current !== identifier) return;
-    },
-    onPanResponderEnd: (e) => {
-      const { pageX, pageY, identifier } = e.nativeEvent;
-      if (touchID.current !== identifier) return;
-      touchID.current = "-1";
-    }
-  })
-
+export const ChoiceSlider = <T extends any>(props: ChoiceSliderProps<T>) => {
+  const {
+    choices,
+    microChunkInterpolator = defaultMicroChunkInterpolator,
+    chunkInterpolator = defaultChunkInterpolator,
+    alignChunks,
+    ..._props
+  } = props
   return (
-    <View {...props}>
-      <animated.View {...panResponder.panHandlers}>
-        <Text style={{color: "white", fontSize: 30}}>
-          {choices[index]}
-        </Text>
-      </animated.View>
-    </View>
+    <CarouselBase
+      {..._props}
+      items={choices}
+      infinite={false}
+      renderItem={({ item, itemPosition, index, info }) => {
+        const chunkTexts = item.map((chunk) => {
+          if (Array.isArray(chunk)) return chunk.map(mapChunkBaseToString).join(" ")
+          return mapChunkBaseToString(chunk)
+        })
+
+        const textChunks = item.map((chunk) => {
+          if (Array.isArray(chunk)) return chunk.map(mapChunkBaseToString).map((_, i) => i > 0 ? " " + _ : _)
+          return [mapChunkBaseToString(chunk)]
+        })
+
+        const [trail, _] = useTrail(item.length, (i) => {
+          const { config, from, ...style } = mapChunkBaseToInterpolation({
+            chunk: textChunks[i],
+            chunkTexts,
+            chunkIndex: i,
+            chunkInterpolator,
+            index,
+            info,
+            item,
+            itemPosition,
+          })
+          return {
+            config,
+            from,
+            ...style,
+          }
+        })
+
+        const springs = item.map((chunk, chunkIndex) => {
+          if (Array.isArray(chunk)) {
+            return chunk.map((_c, i) => mapChunkBaseToInterpolation({
+              chunkIndex, chunk, chunkChildIndex: i,
+              chunkInterpolator: microChunkInterpolator, chunkTexts,
+              index, info, item, itemPosition,
+            }))
+          }
+
+          return [mapChunkBaseToInterpolation({
+            chunkIndex, chunk, chunkChildIndex: -1,
+            chunkInterpolator: microChunkInterpolator, chunkTexts,
+            index, info, item, itemPosition,
+          })]
+        })
+
+        return (
+          <View
+            key={index}
+            style={{
+              //@ts-ignore
+              userSelect: "none",
+              width: "100%",
+              height: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+              position: "absolute",
+            }}>
+            <View style={{ alignItems: alignChunks }}>
+              {textChunks.map((chunk, i) => {
+                const {
+                  plainStyles,
+                  shadowOffset,
+                  transform
+                } = extractCustomKeysFromFlatStyle(trail[i])
+                return (
+                  <animated.View
+                    key={i}
+                    //@ts-ignore
+                    style={{
+                      ...plainStyles,
+                      shadowOffset,
+                      transform,
+                      flexDirection: "row",
+                    }}
+                  >
+                    {chunk.map((text, j) => {
+                      const spring = springs[i][j]
+                      const {
+                        plainStyles,
+                        shadowOffset,
+                        textShadowOffset,
+                        transform
+                      } = extractCustomKeysFromFlatStyle(spring)
+
+                      return (
+                        <animated.Text
+                          key={j}
+                          //@ts-ignore
+                          style={{
+                            ...plainStyles,
+                            shadowOffset,
+                            textShadowOffset,
+                            transform
+                          }}>
+                          {text}
+                        </animated.Text>
+                      )
+                    })}
+                  </animated.View>
+                )
+              })}
+            </View>
+          </View>
+        )
+      }}
+    />
   )
 }
 
-interface ChoiceSliderProps extends ViewProps {
+const fontWeightToNumber = (weight: TextStyle["fontWeight"]) => {
+  if (weight === "bold") return 700
+  if (weight === "normal") return 400
+  return Number(weight)
+}
 
+const mapChunkBaseToString = <T extends any>(c: ChunkBase<T>) => {
+  if (typeof c === "string") return c
+  return c.value
+}
+
+const extractPropsFromChunkBase = <T extends any>(c: ChunkBase<T>) => {
+  if (typeof c === "string") return {}
+  return c.props || {}
+}
+
+type SpringConfigExtractorParams<T> = {
+  chunkIndex: number,
+  chunk: ChunkBase<T> | ChunkBase<T>[],
+  chunkChildIndex?: number,
+  chunkInterpolator: ChunkInterpolatorFn<T>,
+  info: CarouselBaseInterpolatorInfo,
+  item: Choice<T>,
+  index: number,
+  itemPosition: Interpolation<number, number>,
+  chunkTexts: string[]
+}
+
+const mapChunkBaseToInterpolation = <T extends any>({
+  chunk,
+  chunkIndex,
+  chunkInterpolator,
+  chunkTexts,
+  chunkChildIndex = 0,
+  index,
+  info,
+  item,
+  itemPosition,
+}: SpringConfigExtractorParams<T>) => {
+  const chunkText = (() => {
+    return Array.isArray(chunk)
+      ? chunk.map(mapChunkBaseToString).join(" ")
+      : mapChunkBaseToString(chunk)
+  })()
+
+  const text = (() => {
+    return Array.isArray(chunk)
+      ? mapChunkBaseToString(chunk[chunkChildIndex])
+      : chunkText
+  })()
+
+  const props = (() => {
+    return Array.isArray(chunk)
+      ? extractPropsFromChunkBase(chunk[chunkChildIndex])
+      : extractPropsFromChunkBase(chunk)
+  })()
+
+  const { config, from, ...styles } = chunkInterpolator({
+    info,
+    item,
+    itemIndex: index,
+    itemPosition,
+    chunkIndex,
+    chunk: chunkText,
+    chunks: chunkTexts,
+    text,
+    props,
+  })
+
+  const reduced = Object.keys(styles)
+    .map((key) => {
+      const assertedKey = key as keyof typeof styles
+      const output = styles[assertedKey] as NonNullable<typeof styles[typeof assertedKey]>
+
+      if (!Array.isArray(output)) return [assertedKey, output] as const
+
+      const interpolation = (() => {
+        if (key === "fontWeight") {
+          return itemPosition.to((position) => {
+            const numberWeights = (output as TextStyle["fontWeight"][]).map(fontWeightToNumber)
+            const origin = Number(numberWeights[1])
+            const left = Number(numberWeights[0])
+            const right = Number(numberWeights[2])
+            const interpolated = position >= 0
+              ? origin + Math.floor((right - origin) * position / 100) * 100
+              : origin + Math.floor((origin - left) * position / 100) * 100
+            const clamped = Math.max(100, Math.min(interpolated, 900))
+            return String(clamped)
+          })
+        }
+
+        if (textStyleProperties.nonInterpolable.includes(key)) {
+          return itemPosition.to((position) => {
+            const floor = Math.floor(position)
+            return output[floor + 1]
+          })
+        }
+
+        return itemPosition.to({
+          //@ts-ignore
+          range: [-1, 0, 1],
+          output,
+        })
+      })()
+
+      return [assertedKey, interpolation] as const
+    })
+    .reduce((acc, [key, interpolation]) => {
+      Object.assign(acc, { [key]: interpolation })
+      return acc
+    }, {} as {
+      [K in keyof FlatTextStyle]: Interpolation<number, FlatTextStyle[K]> | FlatTextStyle[K]
+    })
+
+  return {
+    ...reduced,
+    config,
+    from,
+  }
+}
+
+function extractCustomKeysFromFlatStyle
+  <T extends Partial<Record<keyof MicroChunkInterpolatorConfig, any>>>
+  (style: T) {
+  const {
+    perspective,
+    translateX, translateY,
+    rotate, rotateX, rotateY, rotateZ,
+    skewX, skewY,
+    scale, scaleY, scaleX,
+    shadowOffsetX, shadowOffsetY,
+    textShadowOffsetX, textShadowOffsetY,
+    config, from,
+    ...plainStyles
+  } = style
+
+  const transform = {
+    perspective,
+    translateX, translateY,
+    rotate, rotateX, rotateY, rotateZ,
+    skewX, skewY,
+    scale, scaleY, scaleX,
+  }
+
+  const textShadowOffset = {
+    width: textShadowOffsetX,
+    height: textShadowOffsetY,
+  }
+
+  const shadowOffset = {
+    width: shadowOffsetX,
+    height: shadowOffsetY,
+  }
+
+  return {
+    transform: Object.keys(transform)
+      .filter((key): key is keyof FlatTransform => {
+        return transform[key as keyof FlatTransform] !== undefined
+      })
+      .map((key) => ({ [key]: transform[key] })),
+    shadowOffset,
+    textShadowOffset,
+    plainStyles,
+  }
+}
+
+export type ChoiceChunkInterpolatorInfo<T> = Omit<CarouselBaseRenderItemInfo<Choice<T>>, "index"> & {
+  itemIndex: number
+  chunkIndex: number
+  text: string
+  chunk: string
+  chunks: string[]
+  props: Partial<T>
+}
+
+type FlatTextStyleKeys = keyof Omit<TextStyle,
+  "transform" |
+  "shadowOffset" |
+  "matrix" |
+  "textShadowOffset"
+>
+
+type FlatViewStyleKeys = keyof Omit<ViewStyle,
+  "transform" |
+  "shadowOffset" |
+  "matrix" |
+  "textShadowOffset"
+>
+
+type FlatTransform = {
+  translateX: number
+  translateY: number
+  rotate: string
+  rotateX: string
+  rotateY: string
+  rotateZ: string
+  skewX: string
+  skewY: string
+  perspective: number
+  scale: number
+  scaleX: number
+  scaleY: number
+}
+
+type FlatViewStyle = {
+  [K in FlatViewStyleKeys]: ViewStyle[K]
+} & FlatTransform
+  & {
+    shadowOffsetX: number
+    shadowOffsetY: number
+  }
+
+type FlatTextStyle = {
+  [K in FlatTextStyleKeys]: TextStyle[K]
+} & FlatTransform
+  & {
+    shadowOffsetX: number
+    shadowOffsetY: number
+    textShadowOffsetX: number
+    textShadowOffsetY: number
+  }
+
+type MicroChunkInterpolatorConfig = {
+  from?: Partial<FlatTextStyle>
+  config?: SpringConfig
+} & {
+    [K in keyof FlatTextStyle]?: [
+      NonNullable<FlatTextStyle[K]>,
+      NonNullable<FlatTextStyle[K]>,
+      NonNullable<FlatTextStyle[K]>,
+    ] | NonNullable<FlatTextStyle[K]>
+  }
+
+type ChunkInterpolatorConfig = {
+  from?: Partial<FlatViewStyle>
+  config?: SpringConfig
+} & {
+    [K in keyof FlatViewStyle]?: [
+      NonNullable<FlatViewStyle[K]>,
+      NonNullable<FlatViewStyle[K]>,
+      NonNullable<FlatViewStyle[K]>,
+    ] | NonNullable<FlatViewStyle[K]>
+  }
+
+
+
+type ChunkBase<T> = (
+  string | { value: string, props?: T }
+)
+type Choice<T> = (ChunkBase<T> | ChunkBase<T>[])[]
+
+type MicronChunkInterpolatorFn<T> = (info: ChoiceChunkInterpolatorInfo<T>) => MicroChunkInterpolatorConfig
+type ChunkInterpolatorFn<T> = (info: ChoiceChunkInterpolatorInfo<T>) => ChunkInterpolatorConfig
+
+export interface ChoiceSliderProps<T> extends ViewProps {
+  alignChunks?: ViewStyle["alignItems"]
+  choices: Choice<T>[]
+  chunkInterpolator?: ChunkInterpolatorFn<T>
+  microChunkInterpolator?: MicronChunkInterpolatorFn<T>
 }
