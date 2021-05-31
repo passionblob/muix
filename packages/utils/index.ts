f15fc721f4176e9b8fd359115fda3962dd59d411
monthem <lpohe01@gmail.com>
2021-05-31 14:24:10 +0900

    chore-utils: shift directory structure

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,16 +1,2 @@
-export * from './event'
-export * from './array'
-export * from "./object"
-export * from "./style"
-
-import * as event from './event'
-import * as array from './array'
-import * as object from "./object"
-import * as style from "./style"
-
-export default {
-  event: event,
-  array: array,
-  obejct: object,
-  style: style,
-}
+export * from "./src"
+export {default as default} from "./src"
---
+++ packages/utils/src/index.ts
@@ -0,0 +1,16 @@
+export * from './event'
+export * from './array'
+export * from "./object"
+export * from "./style"
+
+import * as event from './event'
+import * as array from './array'
+import * as object from "./object"
+import * as style from "./style"
+
+export default {
+  event: event,
+  array: array,
+  obejct: object,
+  style: style,
+}
